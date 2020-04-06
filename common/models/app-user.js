ecd80cf03286fdb18f76f33835da94fcaf74a1df
Sean <cantosean@gmail.com>
2020-04-05 20:31:19 -0700

    backend is all set

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,11 +1,26 @@
 'use strict';
 
 module.exports = function(Appuser) {
-    Appuser.afterRemote('login',async(ctx, user, next)=>{
+    Appuser.afterRemote('login', async (ctx, user, next) => {
         if (user) {
             user.token = user.id;
         }
+        let userData = await Appuser.find({
+            fields:{password: false, username: false, realm: false, '_id': 0},
+            include:{
+              relation: 'favorites',
+              scope: {
+                fields: ['movieTitle', 'thirdPartyMovieId', 'movieGenre', 'overview', 'id']
+              },
+            },
+            where: {
+              id: user.userId
+            }
+          })
+          console.log()
+          user.userData = userData[0]
     })
+    
     Appuser.observe('after save', function(ctx, next) {
         if (ctx.isNewInstance === true) {
         var instance = ctx.instance;
